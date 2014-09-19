33837b285ee941a32736ff3a4d34116715b6fade
Zeatual Chang <zeatualchang@gmail.com>
2014-09-20 02:07:08 +0800

    base style and base structure

---
+++ app/src/main/java/com/china3s/android/activity/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.china3s.android.activity;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+/**
+ * Created by ZZQ。 on 2014/9/20.
+ */
+public class BaseActivity extends SherlockActivity {
+}
---
+++ app/src/main/java/com/china3s/android/fragment/BaseFragment.java
@@ -0,0 +1,9 @@
+package com.china3s.android.fragment;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+/**
+ * Created by ZZQ。 on 2014/9/20.
+ */
+public class BaseFragment extends SherlockFragment {
+}
